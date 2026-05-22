8399da79325a7256990d685af75d5c7047891d5f
Scott Christie <103422729+scottiee-jc@users.noreply.github.com>
2026-05-22 20:00:26 +0000

    first commit

--- main.sql
+++ main.sql
@@ -1,6 +1,4 @@
 
--- Single Table Lab Sheet
-
 CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE);
 
 . schema
